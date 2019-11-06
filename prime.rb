860e1257a621b9ccb52e4a4531eedd875c6d928c
Nethelbert <gilblasse@gmail.com>
2019-11-06 06:26:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime? (num)
 		count = num
 		divisible = 0
 		while count > 0
-				divisible += 1 if (num % count) == 0 && (num <=> num.abs) == 0
+				divisible += 1 if (num % count) == 0
 			count -= 1
 		end
 		divisible <= 2 ? true : false
