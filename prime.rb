d9e4985714b0b3a759592bb677e9934881a42253
Nethelbert <gilblasse@gmail.com>
2019-11-06 06:25:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime? (num)
+  if (num <=> num.abs) == 0 && num != 0 && num != 1
+		count = num
+		divisible = 0
+		while count > 0
+				divisible += 1 if (num % count) == 0 && (num <=> num.abs) == 0
+			count -= 1
+		end
+		divisible <= 2 ? true : false
+	else
+		false
+	end
+end
